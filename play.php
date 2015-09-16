f6a9914e7b3f6c16cbf072b0871a3e6e8932d48d
Angeliki <angel.kastanis@gmail.com>
2015-09-16 15:51:48 -0700

    Update play.php

--- play.php
+++ play.php
@@ -1,1 +1,438 @@
 
+<html><head>
+    <script type="text/javascript">var _sf_startpt=(new Date()).getTime()</script>
+
+    <title>MASH (playing)</title>
+
+    <meta name="description" content="You played it in study hall. You played it at slumber parties. Now it's time to play MASH on the web!" />
+    <link rel="shortcut icon" href="http://www.ilovemash.com/images/heart.png" />
+
+    <link rel="stylesheet" type="text/css" href="facebox/facebox.css" />
+    <style type="text/css">
+    body { margin: 13px 0 0 0; background-image: url(images/background.gif); font-family: helvetica, arial, sans-serif; }
+    p { color: #333; font-size: 12px; line-height: 14px; }
+    .highlight { font-weight: bold; color: #C36; }
+    h1 { margin: 10px 0 0 0; font-weight: bold; color: #333; }
+    #idols { width: 68px; float: left; }
+    #controls { width: 65px; float: left; text-align: center; }
+    #controls img { cursor: pointer; }
+    #main { text-align: center; margin-left: 133px; width: 465px; height: 600px; }
+    #seed-counter { width: 100%; text-align: left; margin-left: 12px; }
+    #mash { margin-bottom: 20px; }
+    #mash-tickers { padding-left: 38px; }
+    #mash-tickers img { display: inline-block; margin: 0; padding: 0 5px 0 5px; }
+    .choice-bucket { text-align: left; margin-left: 15px; width: 200px; height: 130px; float: left; }
+    .choice-bucket h4 { margin: 10px 0 5px 15px;}
+    .choice-bucket p { margin: 0; }
+    #facebox .mash-results { padding: 20px; }
+    #facebox .mash-results p { font-size: 12px; line-height: 14px; }
+    #facebox .mash-results h4 { color: #C36; font-size: 20px; text-transform: uppercase; margin-bottom: 15px; }
+    #facebox .mash-results h4 img { vertical-align: middle; display: inline-block; }
+    #fb-share { color: #C36; }
+    </style>
+
+    <script>
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', 'UA-23701833-1']);
+    _gaq.push(['_trackPageview', '/playing']);
+    </script>
+</head><body>
+
+    <!-- Google Tag Manager -->
+    <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-JJB7"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-JJB7');</script>
+    <!-- End Google Tag Manager -->
+
+    <div id="fb-root"></div>
+
+    <div id="idols">
+        <img src="images/idols.gif" width="68" height="608" />
+    </div>
+
+    <div id="controls">
+        <img id="control-start" src="images/start.gif" width="59" height="24" />
+        <img id="control-stop" src="images/stop.gif" width="59" height="20" />
+        <img id="control-play" src="images/play.gif" width="59" height="20" />
+    </div>
+
+    <div id="main">
+        <div id="main-inner">
+
+            <div id="seed-counter">
+                <img id="seed-1" src="images/blank.gif" width="6" height="10" />
+                <img id="seed-2" src="images/blank.gif" width="6" height="10" />
+                <img id="seed-3" src="images/blank.gif" width="6" height="10" />
+                <img id="seed-4" src="images/blank.gif" width="6" height="10" />
+                <img id="seed-5" src="images/blank.gif" width="6" height="10" />
+                <img id="seed-6" src="images/blank.gif" width="6" height="10" />
+                <img id="seed-7" src="images/blank.gif" width="6" height="10" />
+                <img id="seed-8" src="images/blank.gif" width="6" height="10" />
+                <img id="seed-9" src="images/blank.gif" width="6" height="10" />
+                <img id="seed-10" src="images/blank.gif" width="6" height="10" />
+                <img id="seed-11" src="images/blank.gif" width="6" height="10" />
+                <img id="seed-12" src="images/blank.gif" width="6" height="10" />
+                <img id="seed-13" src="images/blank.gif" width="6" height="10" />
+                <img id="seed-14" src="images/blank.gif" width="6" height="10" />
+                <img id="seed-15" src="images/blank.gif" width="6" height="10" />
+                <img id="seed-16" src="images/blank.gif" width="6" height="10" />
+                <img id="seed-17" src="images/blank.gif" width="6" height="10" />
+                <img id="seed-18" src="images/blank.gif" width="6" height="10" />
+                <img id="seed-19" src="images/blank.gif" width="6" height="10" />
+                <img id="seed-20" src="images/blank.gif" width="6" height="10" />
+                <img id="seed-21" src="images/blank.gif" width="6" height="10" />
+                <img id="seed-22" src="images/blank.gif" width="6" height="10" />
+                <img id="seed-23" src="images/blank.gif" width="6" height="10" />
+                <img id="seed-24" src="images/blank.gif" width="6" height="10" />
+                <img id="seed-25" src="images/blank.gif" width="6" height="10" />
+            </div>
+
+            <div id="mash">
+                <h1>I <span class="highlight">&#x2665;</span> MASH!</h1>
+
+                <div id="mash-tickers">
+                    <img class="ticker" data-value="mansion" id="d-0-0" src="images/blank.gif" width="13" height="12" /><img class="ticker" data-value="apartment" id="d-0-1" src="images/blank.gif" width="13" height="12" /><img class="ticker" data-value="shack" id="d-0-2" src="images/blank.gif" width="13" height="12" /><img class="ticker" data-value="house" id="d-0-3" src="images/blank.gif" width="13" height="12" />
+                </div>
+            </div>
+
+
+		    <div class="choice-bucket">
+                <h4><img src="images/girls.gif" /></h4>
+
+                    <p>
+                    <img class="ticker" data-value="ss" id="d-1-0" src="images/blank.gif" width="13" height="12" />
+                    ss                </p>
+                    <p>
+                    <img class="ticker" data-value="s" id="d-1-1" src="images/blank.gif" width="13" height="12" />
+                    s                </p>
+                    <p>
+                    <img class="ticker" data-value="s" id="d-1-2" src="images/blank.gif" width="13" height="12" />
+                    s                </p>
+                </div>
+
+    
+		    <div class="choice-bucket">
+                <h4><img src="images/cars.gif" /></h4>
+
+                    <p>
+                    <img class="ticker" data-value="s" id="d-2-0" src="images/blank.gif" width="13" height="12" />
+                    s                </p>
+                    <p>
+                    <img class="ticker" data-value="a" id="d-2-1" src="images/blank.gif" width="13" height="12" />
+                    a                </p>
+                    <p>
+                    <img class="ticker" data-value="a" id="d-2-2" src="images/blank.gif" width="13" height="12" />
+                    a                </p>
+                </div>
+
+    
+		    <div class="choice-bucket">
+                <h4><img src="images/color.gif" /></h4>
+
+                    <p>
+                    <img class="ticker" data-value="z" id="d-3-0" src="images/blank.gif" width="13" height="12" />
+                    z                </p>
+                    <p>
+                    <img class="ticker" data-value="d" id="d-3-1" src="images/blank.gif" width="13" height="12" />
+                    d                </p>
+                    <p>
+                    <img class="ticker" data-value="s" id="d-3-2" src="images/blank.gif" width="13" height="12" />
+                    s                </p>
+                </div>
+
+    
+		    <div class="choice-bucket">
+                <h4><img src="images/job.gif" /></h4>
+
+                    <p>
+                    <img class="ticker" data-value="a" id="d-4-0" src="images/blank.gif" width="13" height="12" />
+                    a                </p>
+                    <p>
+                    <img class="ticker" data-value="z" id="d-4-1" src="images/blank.gif" width="13" height="12" />
+                    z                </p>
+                    <p>
+                    <img class="ticker" data-value="a" id="d-4-2" src="images/blank.gif" width="13" height="12" />
+                    a                </p>
+                </div>
+
+    
+		    <div class="choice-bucket">
+                <h4><img src="images/kids.gif" /></h4>
+
+                    <p>
+                    <img class="ticker" data-value="d" id="d-5-0" src="images/blank.gif" width="13" height="12" />
+                    d                </p>
+                    <p>
+                    <img class="ticker" data-value="d" id="d-5-1" src="images/blank.gif" width="13" height="12" />
+                    d                </p>
+                    <p>
+                    <img class="ticker" data-value="d" id="d-5-2" src="images/blank.gif" width="13" height="12" />
+                    d                </p>
+                </div>
+
+    
+		    <div class="choice-bucket">
+                <h4><img src="images/residence.gif" /></h4>
+
+                    <p>
+                    <img class="ticker" data-value="a" id="d-6-0" src="images/blank.gif" width="13" height="12" />
+                    a                </p>
+                    <p>
+                    <img class="ticker" data-value="s" id="d-6-1" src="images/blank.gif" width="13" height="12" />
+                    s                </p>
+                    <p>
+                    <img class="ticker" data-value="d" id="d-6-2" src="images/blank.gif" width="13" height="12" />
+                    d                </p>
+                </div>
+
+    
+        </div>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.5.0/jquery.min.js"></script>
+    <script src="http://connect.facebook.net/en_US/all.js"></script>
+    <script src="mustache.js"></script>
+    <script src="facebox/facebox.js"></script>
+    <script>
+    $.facebox.settings.closeImage = 'facebox/closelabel.png';
+    $.facebox.settings.loadingImage = 'facebox/loading.gif';
+
+    var resultsTemplate = "<h4><img src=\"images/heart.png\" width=\"45\" height=\"45\"> Congratulations!</h4><p>Your {{#likegirls}}wife's {{/likegirls}}{{#likeboys}}husband's {{/likeboys}} name is <strong>{{a1}}</strong> and you have <strong>{{a5}}</strong> {{#onekid}}child{{/onekid}}{{^onekid}}children{{/onekid}}. You're a(n) <strong>{{a4}}</strong> who drives to work every day in a <strong>{{a3}} {{a2}}</strong>.</p><p>It's truly a wonderful life when you consider the countless romantic nights you have spent with <strong>{{a1}}</strong> in your <strong>{{a0}}</strong> in <strong>{{a6}}</strong>.</p>";
+
+    resultsTemplate += '<p><a href="#" id="fb-share">Tell your friends on Facebook!</a>';
+
+    var fbMessageTemplate = 'I played the game of MASH online, and all I got was a marriage to {{a1}}.';
+
+    var Entry = function( value, type, idx ) {
+        this.value = value;
+        this.type = type;
+        this.idx = idx;
+        this.checked = false;
+    }
+
+    Entry.prototype = {
+        list_idx: function() {
+            if (this.type == 0)
+                return this.idx;
+            else
+                return 4 + 3 * (this.type - 1) + this.idx;
+        },
+
+        id: function() {
+            return 'd-' + this.type + '-' + this.idx;
+        }
+    };
+
+    var preload = {};
+    $.each( ['dot', 'blank', 'count', 'count-blank', 'x', 'star'], function() {
+        preload[ this ] = new Image();
+        preload[ this ].src = 'images/' + this + '.gif';
+    } );
+
+    var answers = new Array(6);
+
+    var _run_seed = false;
+    var SEED = 0;
+    $( '#control-start' ).click( function() {
+        _gaq.push(['_trackEvent', 'gameplay', 'pressed-start']);
+
+        // Initialize our seed value, and signal to _doSeed that we
+        // want to start incrementing the seed.
+        SEED = 0;
+        _run_seed = true;
+
+        // Clear out the seed counter images.
+        $( '#seed-counter img' ).each( function() {
+            $( this ).attr( 'src', preload['count-blank'].src );
+        } );
+
+        var _doSeed = function() {
+            // Terminate once the user presses Stop or once we reach
+            // the maximum seed value.
+            if (_run_seed && SEED < 25) {
+                $( '#seed-' + ++SEED ).attr( 'src', preload['count'].src );
+                setTimeout(_doSeed, 200);
+            }
+        };
+
+        _doSeed();
+    } );
+
+    $( '#control-stop' ).click( function() {
+        _run_seed = false;
+    } );
+
+    var _run_play = false;
+    $( '#control-play' ).click( function() {
+        if (!SEED) {
+            alert("You must first press the START button to generate a seed.");
+            return;
+        }
+
+        _gaq.push(['_trackEvent', 'gameplay', 'pressed-play']);
+
+        // Uncheck/unstar all of the items in the list, and build up
+        // a fresh copy of the data.
+        var mashData = [];
+        $( 'img.ticker' ).each( function() {
+            $( this ).attr( 'src', preload['blank'].src );
+            var m = this.id.match( /d-(\d+)-(\d+)/ );
+            mashData.push( new Entry( $( this ).attr( 'data-value' ), parseInt( m[1] ), parseInt( m[2] ) ) );
+        } );
+
+        // Initialize our counters.
+        var tickerCounter = 0,
+            seedCounter = 0,
+            imLast = '',
+            _run_play = true,
+            numOpenTypes = 7;
+
+        var _doPlay = function() {
+            var hilite = 1;
+
+            // Do we have answers for all of the buckets? If so, stop!
+            if ( numOpenTypes == 0 ) {
+                stopPlay();
+                return;
+            }
+
+            var e = mashData[tickerCounter];
+            if (++seedCounter == SEED) {
+                seedCounter = 0;
+                hilite = 0;
+                $( '#d-' + e.type + '-' + e.idx ).attr( 'src', preload['x'].src );
+                mashData[tickerCounter].checked = true;
+
+                // If we've checked off all but one of the choices in this
+                // bucket, we've got an answer. Record it.
+                var answer = haveAnswer(mashData, e.type);
+                if ( typeof answer !== 'undefined' ) {
+                    answers[e.type] = answer.value;
+                    mashData[answer.list_idx()].checked = true;
+                    $( '#' + answer.id() ).attr( 'src', preload['star'].src );
+                    if (imLast == answer.id()) imLast = '';
+                    numOpenTypes--;
+                }
+            }
+            if (imLast) {
+                $( '#' + imLast ).attr( 'src', preload['blank'].src );
+                imLast = '';
+            }
+            if (hilite) {
+                imLast = e.id();
+                $( '#' + imLast ).attr( 'src', preload['dot'].src );
+            }
+            tickerCounter = findNextOpen(mashData, tickerCounter);
+            if (_run_play) {
+                setTimeout(_doPlay, 200);
+            }
+        };
+
+        _doPlay();
+    } );
+
+    function stopPlay () {
+        _run_play = false;
+
+        showResults();
+    }
+
+    function makeView() {
+        var view = { 'likegirls': true };
+        $.each( answers, function( index, value ) {
+            view[ 'a' + index ] = value;
+        } );
+        view['onekid'] = parseInt( view['a5'] ) == 1 ? true : false;
+        return view;
+    }
+
+    function makeResultsHTML () {
+        return Mustache.to_html(resultsTemplate, makeView());
+    }
+
+    function showResults () {
+        _gaq.push(['_trackPageview', '/played']);
+
+        $.facebox( makeResultsHTML(), 'mash-results' );
+    }
+
+    function haveAnswer (list, type) {
+        // Build a list of the unchecked items in the bucket identified
+        // by "type". If there's only one unchecked item, we have an
+        // answer, so return it; otherwise, return undefined.
+        var values = [];
+        $.each( list, function() {
+            if ( this.type == type && !this.checked )
+                values.push( this );
+        } );
+        return values.length == 1 ? values[0] : undefined;
+    }
+
+    function findNextOpen (a, iter) {
+        var sIdx = iter;
+        do {
+            iter++;
+            if (iter >= a.length) iter = 0;
+            if (!a[iter].checked)
+                return iter;
+        } while (iter != sIdx);
+    }
+    </script>
+
+    <script>
+    FB.init({
+        appId: '203945479620956',
+        cookie: true,
+        status: true,
+        xfbml: true
+    });
+    $( '#fb-share' ).live( 'click', function() {
+        _gaq.push(['_trackEvent', 'social', 'facebook-share-started']);
+        FB.ui({
+            method: 'feed',
+            name: 'MASH',
+            link: 'http://www.ilovemash.com/',
+            picture: 'http://www.ilovemash.com/images/heart.png',
+            description: 'You played it in study hall. You played it at slumber parties. Now it\'s time to play MASH on the web!',
+            caption: Mustache.to_html(fbMessageTemplate, makeView())
+        }, function( response ) {
+            if ( response && response.post_id ) {
+                _gaq.push(['_trackEvent', 'social', 'facebook-share-completed']);
+                _gaq.push(['_trackSocial', 'facebook', 'share', 'http://www.ilovemash.com/'])
+            }
+        });
+    } );
+    </script>
+
+    <script type="text/javascript">
+    (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+    </script>
+
+    <script type="text/javascript">
+    var _sf_async_config={uid:8490,domain:"ilovemash.com"};
+    (function(){
+      function loadChartbeat() {
+        window._sf_endpt=(new Date()).getTime();
+        var e = document.createElement('script');
+        e.setAttribute('language', 'javascript');
+        e.setAttribute('type', 'text/javascript');
+        e.setAttribute('src',
+           (("https:" == document.location.protocol) ? "https://a248.e.akamai.net/chartbeat.download.akamai.com/102508/" : "http://static.chartbeat.com/") +
+           "js/chartbeat.js");
+        document.body.appendChild(e);
+      }
+      var oldonload = window.onload;
+      window.onload = (typeof window.onload != 'function') ?
+         loadChartbeat : function() { oldonload(); loadChartbeat(); };
+    })();
+    </script>
+
+</body></html>
